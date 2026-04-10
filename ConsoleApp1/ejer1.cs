edb963734d8c45ed634ac6569825b2c6bbbb220c
cosmicenginath <jurgenplath@gmail.com>
2026-04-10 10:20:46 -0500

    subiendo ejer1 en csharp

---
+++ ConsoleApp1/ejer1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ejer
+    {
+        static void Main(string[] args)
+        {
+            string nm, cr;
+            Console.Write("\nIngrese su nombre:");
+            nm = Console.ReadLine();
+           Console.Write("\nIngrese su carrera:");
+            cr = Console.ReadLine();
+            Console.WriteLine($"Hola <{nm}> ,bienvenido al curso FA, tu carrera es <{cr}>.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ejer2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ejer2
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("\nIngrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("\nHola" + "'nombre'");
+            Console.ReadLine();
+        }
+    }
+}
